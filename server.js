eeb6bc2aad0fb4038cbbe2c7dd4c78ebfb93bbab
Alexis <alexis@Alexiss-MacBook-Pro.local>
2020-06-14 20:17:55 -0400

    mvp

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
-// const actionRouter = require('./routers/actionRouter');
-// const projectRouter = require('./routers/projectRouter');
+const actionRouter = require('./routers/actionRouter');
+const projectRouter = require('./routers/projectRouter');
 
 const server = express();
-
+// endpoint 
 server.use(express.json());
-// server.use('/api/projects', projectRouter);
-// server.use('/api/actions', actionRouter);
+server.use('/api/projects', projectRouter);
+server.use('/api/actions', actionRouter);
 
 
 server.get('/', (req, res) => {
